fd49db7682660902428924af4ffe306a42979010
Diogo Lemos <lemos.diogo105@gmail.com>
2024-02-22 15:11:46 +0000

    Add toString method and test case

--- AED/src/Aula02/QuickFindUF.java
+++ AED/src/Aula02/QuickFindUF.java
@@ -4,8 +4,9 @@ public class QuickFindUF {
     private int[] id;
     
     public QuickFindUF(int N) {
+        id = new int[N];
         if (N < 0) throw new IllegalArgumentException("");
-
+        
         for (int i = 0; i < N; i++) {
             id[i] = i;
         }
@@ -23,5 +24,27 @@ public class QuickFindUF {
                 id[i] = qid;
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < id.length; i++) {
+            s += id[i] + " ";
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        QuickFindUF qf = new QuickFindUF(10);
+        qf.union(4, 3);
+        qf.union(3, 8);
+        qf.union(6, 5);
+        qf.union(9, 4);
+        qf.union(2, 1);
+        qf.union(5, 0);
+        qf.union(7, 2);
+        qf.union(6, 1);
+        System.out.println(qf);
     }
 }
